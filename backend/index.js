9c60ddae8d8e1a2a2aeed5ac95456819905679ad
Leon Matias <leon.matias.dev@gmail.com>
2021-10-18 23:59:19 -0500

    version 03

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');  //procesa imagenes
 const path  = require('path');
+const cors = require('cors');
 
 //Initializations
 const app = express();
@@ -29,6 +30,7 @@ const storage = multer.diskStorage({
 app.use(multer({storage}).single('image'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 //
 //                 // Routes
 //
